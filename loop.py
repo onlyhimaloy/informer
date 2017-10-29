42030efa6e1a171212faee6b849646d67794d145
onlyhimaloy <onlyhimaloy@gmail.com>
2017-10-29 21:17:23 +0900

    loop with argument

---
+++ loop.py
@@ -0,0 +1,21 @@
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done" : break
+    #print(num)
+    try:
+        val = int(num)
+    except:
+        print('invalid input')
+        continue
+
+    if largest is None :
+        largest = num
+    elif num > largest :
+        print("Minimum is ", smallest)
+    if smallest is None :
+         smallest = num
+    elif num < smallest :
+        print("Maximum is ", largest)
+
